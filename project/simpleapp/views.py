f3f961e9995ff00a820ec799bb4013d864b75820
AlexKhudyakov83 <askhudyakov83@yandex.ru>
2024-02-04 20:03:06 +0300

    коммит4

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Product
 from pprint import pprint
+from django.http import HttpResponse
+from .filters import ProductFilter
 
 
 class ProductsList(ListView):
@@ -18,6 +20,26 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    paginate_by = 2
+
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
 
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
@@ -27,12 +49,8 @@ class ProductsList(ListView):
         # что и были переданы нам.
         # В ответе мы должны получить словарь.
         context = super().get_context_data(**kwargs)
-        # К словарю добавим текущую дату в ключ 'time_now'.
-        context['time_now'] = datetime.utcnow()
-        # Добавим ещё одну пустую переменную,
-        # чтобы на её примере рассмотреть работу ещё одного фильтра.
-        context['next_sale'] = "Распродажа в среду!"
-        pprint(context)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
         return context
 
 
@@ -43,3 +61,16 @@ class ProductDetail(DetailView):
     template_name = 'product.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'product'
+
+
+def multiply(request):
+   number = request.GET.get('number')
+   multiplier = request.GET.get('multiplier')
+
+   try:
+       result = int(number) * int(multiplier)
+       html = f"<html><body>{number}*{multiplier}={result}</body></html>"
+   except (ValueError, TypeError):
+       html = f"<html><body>Invalid input.</body></html>"
+
+   return HttpResponse(html)
